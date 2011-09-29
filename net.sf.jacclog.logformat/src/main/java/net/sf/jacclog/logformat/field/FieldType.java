d15f0f5ab2d3e545f0e0d353b6ed013efdea9dfb
André Rouél <andre.rouel@googlemail.com>
2011-09-29 21:20:14 +0200

    Comments changed

--- net.sf.jacclog.logformat/src/main/java/net/sf/jacclog/logformat/field/FieldType.java
+++ net.sf.jacclog.logformat/src/main/java/net/sf/jacclog/logformat/field/FieldType.java
@@ -18,176 +18,244 @@ package net.sf.jacclog.logformat.field;
 public enum FieldType {
 
 	/**
-	 * %...a Remote IP-address
+	 * Remote IP-address<br>
+	 * <br>
+	 * Placeholder: <code>%a</code>
 	 */
 	REMOTE_IP_ADDRESS("a", false),
 
 	/**
-	 * %...A Local IP-address
+	 * Local IP-address <br>
+	 * <br>
+	 * Placeholder: <code>%A</code>
 	 */
 	LOCAL_IP_ADDRESS("A", false),
 
 	/**
-	 * %...B Size of response in bytes, excluding HTTP headers.
+	 * Size of response in bytes, excluding HTTP headers. <br>
+	 * <br>
+	 * Placeholder: <code>%B</code>
 	 */
 	RESPONSE_IN_BYTES("B", false),
 
 	/**
-	 * %...b Size of response in bytes, excluding HTTP headers. In CLF format, i.e. a '-' rather than a 0 when no bytes
-	 * are sent.
+	 * Size of response in bytes, excluding HTTP headers. In CLF format, i.e. a '-' rather than a 0 when no bytes are
+	 * sent.<br>
+	 * <br>
+	 * Placeholder: <code>%b</code>
 	 */
 	RESPONSE_IN_BYTES_CLF("b", false),
 
 	/**
-	 * %...{Foobar}C The contents of cookie Foobar in the request sent to the server.
+	 * The contents of cookie Foobar in the request sent to the server.<br>
+	 * <br>
+	 * Placeholder: <code>%{Foobar}C</code>
 	 */
 	COOKIE("C", true),
 
 	/**
-	 * %...D The time taken to serve the request, in microseconds.
+	 * The time taken to serve the request, in microseconds.<br>
+	 * <br>
+	 * Placeholder: <code>%D</code>
 	 */
 	REQUEST_IN_MILLIS("D", false),
 
 	/**
-	 * %...{FOOBAR}e The contents of the environment variable FOOBAR
+	 * The contents of the environment variable FOOBAR<br>
+	 * <br>
+	 * Placeholder: <code>%{FOOBAR}e</code>
 	 */
 	ENV_VAR("e", true),
 
 	/**
-	 * %...f Filename
+	 * Filename<br>
+	 * <br>
+	 * Placeholder: <code>%f</code>
 	 */
 	FILENAME("f", false),
 
 	/**
-	 * %...h Remote host
+	 * Remote host<br>
+	 * <br>
+	 * Placeholder: <code>%h</code>
 	 */
 	REMOTE_HOST("h", false),
 
 	/**
-	 * %...H The request protocol
+	 * The request protocol<br>
+	 * <br>
+	 * Placeholder: <code>%H</code>
 	 */
 	REQUEST_PROTOCOL("H", false),
 
 	/**
-	 * %...{Foobar}i The contents of Foobar: header line(s) in the request sent to the server.
+	 * The contents of Foobar: header line(s) in the request sent to the server.<br>
+	 * <br>
+	 * Placeholder: <code>%{Foobar}i</code>
 	 */
 	REQUEST_HEADER_LINE("i", true),
 
 	/**
-	 * %...l Remote logname (from identd, if supplied). This will return a dash unless IdentityCheck is set On.
+	 * Remote logname (from identd, if supplied). This will return a dash unless IdentityCheck is set On.<br>
+	 * <br>
+	 * Placeholder: <code>%l</code>
 	 */
 	REMOTE_LOGNAME("l", false),
 
 	/**
-	 * %...m The request method
+	 * The request method<br>
+	 * <br>
+	 * Placeholder: <code>%m</code>
 	 */
 	REQUEST_METHOD("m", false),
 
 	/**
-	 * %...{Foobar}n The contents of note Foobar from another module.
+	 * The contents of note Foobar from another module.<br>
+	 * <br>
+	 * Placeholder: <code>%{Foobar}n</code>
 	 */
 	MOD_CONTENTS("n", true),
 
 	/**
-	 * %...{Foobar}o The contents of Foobar: header line(s) in the reply.
+	 * The contents of Foobar: header line(s) in the reply.<br>
+	 * <br>
+	 * Placeholder: <code>%{Foobar}o</code>
 	 */
 	RESPONSE_HEADER_LINE("o", true),
 
 	/**
-	 * %...p The canonical port of the server serving the request
+	 * The canonical port of the server serving the request<br>
+	 * <br>
+	 * Placeholder: <code>%p</code>
 	 */
 	SERVER_PORT("p", false),
 
 	/**
-	 * %...P The process ID of the child that serviced the request.
+	 * The process ID of the child that serviced the request.<br>
+	 * <br>
+	 * Placeholder: <code>%P</code>
 	 */
 	PROCESS_ID("P", false),
 
 	/**
-	 * %...{format}P The process ID or thread id of the child that serviced the request. Valid formats are pid and tid.
-	 * (Apache 2.0.46 and later)
+	 * The process ID or thread id of the child that serviced the request. Valid formats are pid and tid. (Apache 2.0.46
+	 * and later)<br>
+	 * <br>
+	 * Placeholder: <code>%{format}P</code>
 	 */
 	PID_OR_TID("P", true),
 
 	/**
-	 * %...q The query string (prepended with a ? if a query string exists, otherwise an empty string)
+	 * The query string (prepended with a <code>?</code> if a query string exists, otherwise an empty string)<br>
+	 * <br>
+	 * Placeholder: <code>%q</code>
 	 */
 	QUERY_STRING("q", false),
 
 	/**
-	 * %...r First line of request
+	 * First line of request<br>
+	 * <br>
+	 * Placeholder: <code>%r</code>
 	 */
 	REQUEST_FIRST_LINE("r", false),
 
 	/**
-	 * %...s Status. For requests that got internally redirected, this is the status of the *original* request ---
-	 * %...>s for the last.
+	 * Status. For requests that got internally redirected, this is the status of the *original* request --- %...>s for
+	 * the last.<br>
+	 * <br>
+	 * Placeholder: <code>%s</code>
 	 */
 	STATUS_CODE("s", false),
 
 	/**
-	 * %...s Status for the last request
+	 * Status for the last request<br>
+	 * <br>
+	 * Will be interpreted as field <code>STATUS_CODE</code>.<br>
+	 * <br>
+	 * Placeholder: <code>%s</code>
 	 */
 	LAST_STATUS_CODE(">s", false),
 
 	/**
-	 * %...t Time the request was received (standard english format)
+	 * Time the request was received (standard english format)<br>
+	 * <br>
+	 * Placeholder: <code>%t</code>
 	 */
 	REQUEST_TIME("t", false),
 
 	/**
-	 * %...{format}t The time, in the form given by format, which should be in strftime(3) format. (potentially
-	 * localized)
+	 * The time, in the form given by format, which should be in strftime(3) format. (potentially localized)<br>
+	 * <br>
+	 * Placeholder: <code>%{format}t</code>
 	 */
 	REQUEST_TIME_CUSTOM("t", true),
 
 	/**
-	 * %...T The time taken to serve the request, in seconds.
+	 * The time taken to serve the request, in seconds.<br>
+	 * <br>
+	 * Placeholder: <code>%T</code>
 	 */
 	REQUEST_IN_SECONDS("T", false),
 
 	/**
-	 * %...u Remote user (from auth; may be bogus if return status (%s) is 401)
+	 * Remote user (from auth; may be bogus if return status (%s) is 401)<br>
+	 * <br>
+	 * Placeholder: <code>%u</code>
 	 */
 	REMOTE_USER("u", false),
 
 	/**
-	 * %...U The URL path requested, not including any query string.
+	 * The URL path requested, not including any query string.<br>
+	 * <br>
+	 * Placeholder: <code>%U</code>
 	 */
 	URL_PATH("U", false),
 
 	/**
-	 * %...v The canonical ServerName of the server serving the request.
+	 * The canonical ServerName of the server serving the request.<br>
+	 * <br>
+	 * Placeholder: <code>%v</code>
 	 */
 	SERVERNAME_CANONICAL("v", false),
 
 	/**
-	 * %...V The server name according to the UseCanonicalName setting.
+	 * The server name according to the UseCanonicalName setting.<br>
+	 * <br>
+	 * Placeholder: <code>%V</code>
 	 */
 	SERVERNAME("V", false),
 
 	/**
-	 * %...X Connection status when response is completed:
+	 * Connection status when response is completed:
 	 * <ul>
 	 * <li>X = connection aborted before the response completed.</li>
 	 * <li>+ = connection may be kept alive after the response is sent.</li>
 	 * <li>- = connection will be closed after the response is sent.</li>
 	 * </ul>
+	 * <br>
+	 * <br>
+	 * Placeholder: <code>%X</code>
 	 */
 	CONNECTION_STATUS("X", false),
 
 	/**
-	 * %...I Bytes received, including request and headers, cannot be zero. You need to enable mod_logio to use this.
+	 * Bytes received, including request and headers, cannot be zero. You need to enable <i>mod_logio</i> to use this.<br>
+	 * <br>
+	 * Placeholder: <code>%I</code>
 	 */
 	BYTES_RECEIVED("I", false),
 
 	/**
-	 * %...O Bytes sent, including headers, cannot be zero. You need to enable mod_logio to use this.
+	 * Bytes sent, including headers, cannot be zero. You need to enable <i>mod_logio</i> to use this.<br>
+	 * <br>
+	 * Placeholder: <code>%O</code>
 	 */
 	BYTES_SEND("O", false),
 
 	/**
-	 * A common slot for ignorable fields, can be applied multiple times
+	 * A common slot for ignorable fields, can be applied multiple times <br>
+	 * <br>
+	 * Placeholder: <code>%0</code>
 	 */
 	IGNORE("0", false);
 
