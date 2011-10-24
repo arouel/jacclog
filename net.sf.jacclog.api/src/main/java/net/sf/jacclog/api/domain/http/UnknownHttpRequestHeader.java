42a9b76d85720dd3761626e139af644fe7617bc5
André Rouél <andre.rouel@googlemail.com>
2011-10-24 23:47:21 +0200

    Added important methods hashCode(), equals() and toString()

--- net.sf.jacclog.api/src/main/java/net/sf/jacclog/api/domain/http/UnknownHttpRequestHeader.java
+++ net.sf.jacclog.api/src/main/java/net/sf/jacclog/api/domain/http/UnknownHttpRequestHeader.java
@@ -28,6 +28,28 @@ public class UnknownHttpRequestHeader implements ReadableHttpRequestHeader {
 		this.name = name;
 	}
 
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final UnknownHttpRequestHeader other = (UnknownHttpRequestHeader) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
 	/**
 	 * Gets the name of the unknown (probably non-standardized) request header which is never <code>null</code>.
 	 * 
@@ -36,6 +58,23 @@ public class UnknownHttpRequestHeader implements ReadableHttpRequestHeader {
 	@Override
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("UnknownHttpRequestHeader [name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
